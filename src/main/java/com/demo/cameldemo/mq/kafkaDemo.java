e5507446a71eb86c9441c80ca74fae5ce1160db4
hhhyde <333333>
2019-02-12 18:02:58 +0800

    kafka组件

--- src/main/java/com/demo/cameldemo/mq/kafkaDemo.java
+++ src/main/java/com/demo/cameldemo/mq/kafkaDemo.java
@@ -11,11 +11,12 @@ public class kafkaDemo extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("timer://my-timer?repeatCount=2")
+                .log("#######")
                 .setBody(constant("Message from Camel"))          // Message to send
                 .setHeader(KafkaConstants.KEY, constant("Camel")) // Key of the message
-                .to("kafka:test?brokers=119.27.161.183:9092");
+                .to("kafka:119.27.161.183:9092?topic=test");
 
-        from("kafka:test?groupId=A&brokers=119.27.161.183:9092")
+        from("kafka:test?topic=test&brokers=119.27.161.183:9092&groupId=test")
                 .log("Message received from Kafka : ${body}")
                 .log("    on the topic ${headers[kafka.TOPIC]}")
                 .log("    on the partition ${headers[kafka.PARTITION]}")
