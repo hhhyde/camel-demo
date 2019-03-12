e4885b3fc595c1cbfe46c18f37b7307ebba14a16
hhhyde <kj8550@gmail.com>
2019-03-12 21:10:44 +0800

    sql 批量执行

--- src/main/java/com/demo/cameldemo/jdbc/JDBC批量执行.java
+++ src/main/java/com/demo/cameldemo/jdbc/JDBC批量执行.java
@@ -50,27 +50,24 @@ public class JDBC批量执行 extends RouteBuilder {
 		// TODO: 2019-03-09 多sql事务无法共享
 		from("timer://批量执行效率?repeatCount=1")
 				.onCompletion().process(exchange -> {
-			System.out.println(String.format("同步结束,数据量:%s,耗时:%dms", exchange.getIn().getHeader("CamelJdbcRowCount"), System.currentTimeMillis() - (long) exchange.getProperty("CostTime")));
+			System.out.println(String.format("同步结束,数据量:%s,耗时:%dms", exchange.getIn().getHeader("CamelSqlRowCount"), System.currentTimeMillis() - (long) exchange.getProperty("CostTime")));
 		})
 				.end()
 				.process(ex -> {
 					ex.setProperty("CostTime", System.currentTimeMillis());
 					System.out.println("###### 开始同步!" + ex.getExchangeId());
 				})
-				.setBody(constant("select * from SYS_USER"))
-				.to("jdbc:49")
+				.to("sql:select * from SYS_USER?dataSource=49")
 				.process(exchange -> {
-					System.out.println(String.format("查询结束,数据量:%s,耗时:%dms", exchange.getIn().getHeader("CamelJdbcRowCount"), System.currentTimeMillis() - (long) exchange.getProperty("CostTime")));
+					System.out.println(String.format("查询结束,数据量:%s,耗时:%dms", exchange.getIn().getHeader("CamelSqlRowCount"), System.currentTimeMillis() - (long) exchange.getProperty("CostTime")));
 				})
-//				.split(body()) // 加这个就没有批量的效果了
-//				.threads(20,20) // 目前效果提升不大
-				.to("sql:DELETE FROM SYS_USER_COPY WHERE ID=:#ID?dataSource=183&batch=true&transacted=true") // 数据存在更新时的场景,先删除再新增
-				.to("sql:INSERT INTO SYS_USER_COPY(ID, USERNAME, PASSWORD, CONTENT, INFO, DD, TT) VALUES (:#ID,:#USERNAME,:#PASSWORD,:#CONTENT,:#INFO,:#DD,:#TT)?dataSource=183&batch=true&transacted=true")
+//				.to("sql:DELETE FROM SYS_USER_COPY WHERE ID=:#ID?dataSource=183&batch=true&transacted=true") // 数据存在更新时的场景,先删除再新增
+				.to("sql:INSERT INTO SYS_USER_COPY(ID, USERNAME, PASSWORD, CONTENT, INFO, DD, TT) VALUES (:#ID,:#USERNAME,:#PASSWORD,:#CONTENT,:#INFO,:#DD,:#TT)?dataSource=183&batch=true")
 		;
 	}
 
 	void 注册数据库(String name, String url) {
-		BeanDefinitionBuilder builder = null;
+		BeanDefinitionBuilder builder;
 		builder = BeanDefinitionBuilder.genericBeanDefinition(
 				org.apache.commons.dbcp.BasicDataSource.class);
 		builder.addPropertyValue("initialSize", 5);
@@ -81,8 +78,6 @@ public class JDBC批量执行 extends RouteBuilder {
 
 
 		builder.addPropertyValue("driverClassName", "oracle.jdbc.driver.OracleDriver");
-//        builder.addPropertyValue("url", "jdbc:oracle:thin:@119.27.161.183:49161:XE");
-//        builder.addPropertyValue("url", "jdbc:oracle:thin:@x23178478p.iok.la:1521:ORCL");
 		builder.addPropertyValue("url", url);
 		builder.addPropertyValue("username", "DATAPLATFORM");
 		builder.addPropertyValue("password", "DATAPLATFORM");
